80fe6802ac83baed5cdbb36d559c7f4bbe56485f
vishal <mailtomevishalkv@gmail.com>
2025-06-25 20:27:33 +0530

    added go backend

---
+++ job-app-be/go.mod
@@ -0,0 +1,3 @@
+module github.com/Iamvishal007/job-finder
+
+go 1.24.3
---
+++ job-app-be/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello form the Go backend! Current time: %s", time.Now().Format("2006-01-02 15:04:05"))
+	})
+
+	log.Println("Go server starting on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Server failed ot start: %v", err)
+	}
+}
