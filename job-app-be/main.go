3044cc6eeeb0432522aefe21bae2e0667df7da54
vishal <mailtomevishalkv@gmail.com>
2025-06-29 12:17:17 +0530

    change backend port from 8080 to 8000

--- job-app-be/main.go
+++ job-app-be/main.go
@@ -12,8 +12,8 @@ func main() {
 		fmt.Fprintf(w, "hello form the Go backend! Current time: %s", time.Now().Format("2006-01-02 15:04:05"))
 	})
 
-	log.Println("Go server starting on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("Go server starting on port 8000...")
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
